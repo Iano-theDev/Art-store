22887e5fe0d407692b3284fa7fd961b989eb76a2
Iano-theDev <sinceianmike@gmail.com>
2023-02-12 22:29:53 +0300

    add image ulr column

--- server/databases/tables/productsTable.sql
+++ server/databases/tables/productsTable.sql
@@ -5,5 +5,6 @@ CREATE TABLE Products
     Description VARCHAR(500),
     Category VARCHAR(150) ,
     Price VARCHAR(100),
+    ImageUrl VARCHAR(255) NOT NULL,
     createdAt DATETIME NOT NULL DEFAULT GETDATE()
 );

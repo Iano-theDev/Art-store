31391253d10c65231d9edc465c30b22a1ce75bb8
Iano-theDev <sinceianmike@gmail.com>
2023-02-13 08:08:35 +0300

    Update order table: added order status

--- server/databases/tables/ordersTable.sql
+++ server/databases/tables/ordersTable.sql
@@ -3,6 +3,7 @@ CREATE TABLE Orders
     orderId VARCHAR PRIMARY KEY NOT NULL,
     productId VARCHAR(100) NOT NULL,
     userId VARCHAR(100)NOT NULL,
+    orderStatus VARCHAR(50) NOT NULL CHECK(orderStatus IN('ordered', 'in-transit', 'delivered')),
     FOREIGN KEY (productId) REFERENCES Products(productId),
     FOREIGN KEY (userId) REFERENCES Users(userId),
     createdAt DATETIME NOT NULL DEFAULT GETDATE()

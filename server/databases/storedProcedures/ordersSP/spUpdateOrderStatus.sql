c0a6269dfc0c358a191c2c319d5df749ba341c20
Iano-theDev <sinceianmike@gmail.com>
2023-02-15 23:57:15 +0300

    add all controllers and routes for orders

--- server/databases/storedProcedures/ordersSP/spUpdateOrderStatus.sql
+++ server/databases/storedProcedures/ordersSP/spUpdateOrderStatus.sql
@@ -2,7 +2,7 @@ CREATE PROCEDURE spUpdateOrderStatus(@oId VARCHAR(255), @status VARCHAR(255))
 AS
 BEGIN
     UPDATE Orders
-    SET OrderStatus = @status
+    SET orderStatus = @status
     WHERE OrderId = @oId
 END
 GO
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,7 @@
 import express, { json } from 'express'
 import productsRouter from './routes/productRoutes'
 import cartRoutes from './routes/cartRoutes'
+import orderRoutes from './routes/orderRoutes'
 
 const app = express()
 
@@ -10,6 +11,8 @@ app.use(json())//add a body to the request(previously body parser)
 app.use('/products', productsRouter)
 
 app.use('/cart', cartRoutes)
+
+app.use('/orders', orderRoutes)
 
 app.listen(4000, ()=> {
     console.log(`server running `)

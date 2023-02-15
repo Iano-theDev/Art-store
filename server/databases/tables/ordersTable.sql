f96d3b3aeb507dbe9ff3d33e7ce6fc090cf96c17
Iano-theDev <sinceianmike@gmail.com>
2023-02-15 06:56:18 +0300

    Update Cart table and stored procedures

--- server/databases/tables/ordersTable.sql
+++ server/databases/tables/ordersTable.sql
@@ -1,9 +1,9 @@
 CREATE TABLE Orders
 (
-    orderId VARCHAR PRIMARY KEY NOT NULL,
+    orderId VARCHAR PRIMARY KEY IDENTITY,
     productId VARCHAR(100) NOT NULL,
     userId VARCHAR(100)NOT NULL,
-    orderStatus VARCHAR(50) NOT NULL CHECK(orderStatus IN('ordered', 'in-transit', 'delivered')),
+    orderStatus VARCHAR(50) NOT NULL CHECK(orderStatus IN('ordered', 'in-transit', 'delivered')) DEFAULT 1,
     FOREIGN KEY (productId) REFERENCES Products(productId),
     FOREIGN KEY (userId) REFERENCES Users(userId),
     createdAt DATETIME NOT NULL DEFAULT GETDATE()
